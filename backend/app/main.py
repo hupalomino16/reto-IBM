ecd1d36a7876c7e1bac9a26461f3852f9dec390e
hupalomino16 <58094560+hupalomino16@users.noreply.github.com>
2019-11-23 01:24:40 -0500

    Add files via upload

---
+++ backend/app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/retoibm/sumar/{sumando01}/{sumando02}")
+async def read_item(sumando01 : int,sumando02 : int):
+    return {"resultado": sumando01 + sumando02}
